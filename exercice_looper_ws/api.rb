39c389ffcb06c00d5438711e2b25b6bf4437a63f
PedroSilva02 <pedro02school@gmail.com>
2026-09-01 22:48:33 +0200

    feat: add fetch all questionnaires endpoint

--- exercice_looper_ws/api.rb
+++ exercice_looper_ws/api.rb
@@ -16,13 +16,15 @@ class Api
       if path == "/api/questionnaires" && method == "POST"
         body = env["rack.input"].read
         data = JSON.parse(body)
-
         title = data["title"]
-
         @exercice_looper_service.create_questionnaire(title)
-
         return [201, { "content-type" => "application/json" }, [{ message: "Questionnaire #{title} CREATED " }.to_json]]
-
       end
+
+    if path == "/api/questionnaires" && method == "GET"
+      questionnaires = @exercice_looper_service.fetch_all_questionnaires
+
+      return [201, { "content-type" => "application/json" },  [{ questionnaires: questionnaires }.to_json]]
+    end
   end
 end
--- exercice_looper_ws/service/exercice_looper_service.rb
+++ exercice_looper_ws/service/exercice_looper_service.rb
@@ -7,8 +7,23 @@ class ExerciceLooperService
   def initialize()
     @db = Database.connection
   end
+
   def create_questionnaire(title)
     puts "NOM DU QAuest #{title}"
     @db.prepare("INSERT INTO questionnaires (title) VALUES (?)").execute(title)
+  end
+
+  def fetch_all_questionnaires
+    puts "Fetching all questionnaires"
+    questionnaires = @db.query('SELECT * FROM questionnaires').to_a
+
+    questionnaires.each do |questionnaire|
+      puts questionnaire
+    end
+
+  end
+
+  def create_question(question_text, questionnaire_id, question_type_id)
+
   end
 end
