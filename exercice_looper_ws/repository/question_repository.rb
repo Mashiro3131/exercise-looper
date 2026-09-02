83d94b369a795bb4a6cb6d161a2725d498a9f20d
PedroSilva02 <pedro02school@gmail.com>
2026-09-02 09:55:21 +0200

    feat: add question_repository

--- exercice_looper_ws/repository/question_repository.rb
+++ exercice_looper_ws/repository/question_repository.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class QuestionRepository
+
+  def initialize(db_connection)
+    @db = db_connection
+  end
+
+  def update(question_id, question_text, questionnaire_id, question_type_id)
+    statement = @db.prepare("UPDATE questions SET question_text = ?, questionnaire_id = ?,question_type_id = ?WHERE question_id = ?")
+    statement.execute(question_id, question_text, questionnaire_id, question_type_id)
+  end
+
+  def create(question_text, questionnaire_id, question_type_id)
+    statement = @db.prepare("INSERT INTO questions (question_text, questionnaire_id, question_type_id) VALUES (?, ?, ?)")
+    statement.execute(question_text, questionnaire_id, question_type_id)
+  end
+
+  def find_all_questions_by_questionnaire_id(questionnaire_id)
+    @db.query("SELECT * FROM questions WHERE questionnaire_id = ?").to_a
+  end
 end
